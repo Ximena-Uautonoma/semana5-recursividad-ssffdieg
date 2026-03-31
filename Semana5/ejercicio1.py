e9686db1bf5d62ecc3150311b9baa7aab1517303
ssffdieg <diego.two007@gmail.com>
2026-03-31 17:54:00 +0000

    se eelimino n =5, y se puso el segungo item

--- Semana5/ejercicio1.py
+++ Semana5/ejercicio1.py
@@ -5,7 +5,7 @@ Debe implementar dos funciones:
 1. Una usando iteración (for o while)
 2. Una usando recursividad
 """
-n = 5
+
 def contar_ciclo(n):
     """
     Retorna una lista con los números desde 1 hasta n usando iteración.
@@ -15,7 +15,7 @@ def contar_ciclo(n):
     for i in range (1,n+1):
         numeros.append(i)
     return(numeros)
-print(contar_ciclo)
+print(contar_ciclo(5))
 #se coloco el primer item de ejercicio
 
 
@@ -24,4 +24,10 @@ def contar_recursivo(n):
     Retorna una lista con los números desde 1 hasta n usando recursividad.
     """
     # Escriba aquí su solución y borre la palabra pass de acontinuación
-    pass
+    if n == 1:
+        return [1]
+    else:
+        return contar_recursivo(n-1)+ [n]
+print(contar_recursivo(5))
+    
+
