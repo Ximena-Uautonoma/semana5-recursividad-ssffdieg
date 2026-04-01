e4025adb67fa54489d60e4bf15a45fd02c0a20e6
ssffdieg <diego.two007@gmail.com>
2026-04-01 19:11:06 +0000

    se agrero el codigo y el print

--- Semana5/ejercicio4.py
+++ Semana5/ejercicio4.py
@@ -4,7 +4,12 @@ Dado un número entero positivo N, contar cuántos números pares existen entre 
 """
 
 def contar_pares_ciclo(n):
-    pass
+    contador = 0
+    for i in range(1,n+1):
+        if i % 2 == 0:
+            contador += 1
+    return contador(5)
+print("contar_pares_ciclo: ", contar_pares_ciclo(5))
 
 
 def contar_pares_recursivo(n):
