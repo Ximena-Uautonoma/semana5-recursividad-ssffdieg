65b78c9864841b020ce66aba1a1456603101d840
ssffdieg <diego.two007@gmail.com>
2026-04-02 14:29:04 +0000

    prueba de solucion de errores

--- Semana5/ejercicio3.py
+++ Semana5/ejercicio3.py
@@ -18,5 +18,5 @@ def factorial_recursivo(n):
     if n == 1:
         return 1
     else:
-        return n * factorial_ciclo(n-1)
+        return n * factorial_recursivo(n-1)
 print ("factorial_recursivo: ",factorial_recursivo(5))
